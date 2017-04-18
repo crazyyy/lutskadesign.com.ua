8cef183ced33f2c23dadfddff003846687c67c23
crazyyy <crazyyy@gmail.com>
2017-04-18 18:38:21 +0300

    on vacantion

---
+++ html/js/inline_4_2.js
@@ -0,0 +1,2 @@
+$(function(){$(document.body).on("click touchend","#swipebox-slider .current img",function(c){return!1}).on("click touchend","#swipebox-slider .current",function(c){$("#swipebox-close").trigger("click")})});
+//# sourceMappingURL=maps/inline_4_2.js.map
---
+++ html/js/inline_9.js
@@ -0,0 +1,2 @@
+var icl_vars={current_language:"ru",icl_home:"http://nataly-bolshakova.com.ua/"};
+//# sourceMappingURL=maps/inline_9.js.map
